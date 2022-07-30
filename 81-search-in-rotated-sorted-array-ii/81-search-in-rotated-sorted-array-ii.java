8b0516e1052c8033c7c117b8ea7841c9bf48f901
Akash Menon <74711604+Akash0420@users.noreply.github.com>
2022-07-31 02:37:15 +0530

    Time: 1 ms (86.94%), Space: 44 MB (36.86%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,6 +1,9 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-int start = 0, end = nums.length - 1, mid = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        int mid = -1;
+        
         while(start <= end) {
             mid = (start + end) / 2;
             if (nums[mid] == target) {
@@ -27,7 +30,6 @@ int start = 0, end = nums.length - 1, mid = -1;
                 end--;
             }
         }
-        
         return false;
     }
 }
