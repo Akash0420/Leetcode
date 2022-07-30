df16a72bc400c4c9edb8f33ad6e5c74118871ec5
Akash Menon <74711604+Akash0420@users.noreply.github.com>
2022-07-30 16:28:39 +0530

    Time: 0 ms (100.00%), Space: 48.4 MB (24.02%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.java
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.java
@@ -1,6 +1,31 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
+        char res=letters[0];
         int start=0;
+        int end=letters.length-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(letters[mid]==target)
+            {
+                start=mid+1;
+            }
+            else if(target>letters[mid])
+            {
+                start=mid+1;
+            }
+            else if(letters[mid]>target)
+            {
+                res=letters[mid];
+                end=mid-1;
+            }
+        }
+        return res;
+        
+    }   
+}
+        
+        /*int start=0;
         int end=letters.length-1;
         
         if(target >= letters[end])
@@ -25,4 +50,4 @@ class Solution {
     }
          return letters[start];
 }
-}
+}*/
