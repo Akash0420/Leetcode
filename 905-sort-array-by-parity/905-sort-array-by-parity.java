56609428a9a24d8e446f7be1b44af48e37967ebe
Akash Menon <74711604+Akash0420@users.noreply.github.com>
2022-08-12 19:42:20 +0530

    Time: 1 ms (100.00%), Space: 48.6 MB (29.50%) - LeetHub

---
+++ 905-sort-array-by-parity/905-sort-array-by-parity.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            if (nums[i] % 2 > nums[j] % 2) {
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+            }
+
+            if (nums[i] % 2 == 0) i++;
+            if (nums[j] % 2 == 1) j--;
+        }
+
+        return nums;
+    }
+}
+/*If we want to do the sort in-place, we can use quicksort, a standard textbook algorithm.
+
+Algorithm
+
+We'll maintain two pointers i and j. The loop invariant is everything below i has parity 0 (ie. A[k] % 2 == 0 when k < i), and everything above j has parity 1.
+
+Then, there are 4 cases for (A[i] % 2, A[j] % 2):
+
+If it is (0, 1), then everything is correct: i++ and j--.
+
+If it is (1, 0), we swap them so they are correct, then continue.
+
+If it is (0, 0), only the i place is correct, so we i++ and continue.
+
+If it is (1, 1), only the j place is correct, so we j-- and continue.
+
+Throughout all 4 cases, the loop invariant is maintained, and j-i is getting smaller. So eventually we will be done with the array sorted as desired.*/
