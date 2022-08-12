6699b63c8b2466290a4af2daff0b5bffdcbaa83f
Akash Menon <74711604+Akash0420@users.noreply.github.com>
2022-08-12 17:58:17 +0530

    Time: 7 ms (24.18%), Space: 51.2 MB (39.57%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n=nums.length;
-        for(int i=0;i<n-1;i++){
+        /*for(int i=0;i<n-1;i++){
             boolean swapped=false;
         for(int j=0;j<n-1;j++){
             if(nums[j]>nums[j+1]){
@@ -14,7 +14,8 @@ class Solution {
         }
         if(!swapped)
             break;
-        }
+        }*/
+        Arrays.sort(nums);
         int left = 0, right = nums.length, mid= (left + right)/2;
     while(left<right){
         mid = (left + right)/2;
